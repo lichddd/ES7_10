84f9a615728e95e6a1aa7c5ac159ec8df52ab132
lichddd <lichddd@126.com>
2020-02-13 17:00:43 +0800

    1

--- src/codes.js
+++ src/codes.js
@@ -46,7 +46,7 @@ export default [
                 label:'不使用if-eles按条件创建不同的数组',
                 code:{
                     text:
-                    `function foo(insert_num){
+                    `function foo(insert_num,text){
                         let a=[];
                         a.length=insert_num;
                         a.fill(text);
@@ -65,6 +65,7 @@ export default [
                         {key:'text',value:''},
                     ],
                     func:(result,{insert_num=0,text=''})=>{
+                        result.text='';
                         let a=[];
                         a.length=insert_num;
                         a.fill(text);
@@ -79,6 +80,110 @@ export default [
                     }
                 },
             },
+        ],
+    },
+    {
+        label:'最佳实践',
+        children:[
+            {
+                label:'串行异步调用',
+                code:{
+                    text:
+                    `function foo(loop=1){
+                        function p(){
+                            return new Promise(function(resolve, reject){
+                                setTimeout(resolve, 1000);
+                            });
+                        }
+                        for (let i = 0; i < loop; i++) {
+                            await p();
+                            //输出每次任务完成
+                        }
+                    }
+                    `,
+                    params:
+                    [
+                        {key:'loop',value:1},
+                    ],
+                    func:async (result,{loop=1})=>{
+                        result.text='';
+                        function p(){
+                            return new Promise(function(resolve, reject){
+                                setTimeout(resolve, 1000);
+                            });
+                        }
+                        for (let i = 0; i < loop; i++) {
+                            await p();
+                            result.text+=`任务${i+1}完成\n`;
+                        }
+                        
+                    }
+                },
+            },            
+            {
+                label:'string padding',
+                code:{
+                    text:
+                    `function foo(len=1,char='')=>{
+                        let str='测试文字';
+                        str=str.padStart(len,char);
+                        //输出结果
+                    }
+                    `,
+                    params:
+                    [
+                        {key:'len',value:1},
+                        {key:'char',value:''},
+                    ],
+                    func:(result,{len=1,char=''})=>{
+                        result.text='';
+                        let str='测试文字';
+                        str=str.padStart(len,char);
+                        result.text=str;
+
+                        '123'.padStart(8,0);
+                    }
+                },
+            },    
+            {
+                label:'数组去重',
+                code:{
+                    text:
+                    `function foo()=>{
+                        let arr=[1,2,3,3,3,3,4,4,4,5,6];
+                        let arr1=[1,2,{a:1},{a:1},{a:2}];
+                        let o1={a:1};
+                        let o2={a:2};
+                        let arr2=[1,2,o1,o1,o2];
+
+                        arr=Array.from(new Set(arr));
+                        arr1=Array.from(new Set(arr1));
+                        arr2=Array.from(new Set(arr2));
+                        //输出结果
+                    }
+                    `,
+                    params:
+                    [
+                    ],
+                    func:(result,{})=>{
+                        result.text='';
+                        let arr=[1,2,3,3,3,3,4,4,4,5,6];
+                        let arr1=[1,2,{a:1},{a:1},{a:2}];
+                        let o1={a:1};
+                        let o2={a:2};
+                        let arr2=[1,2,o1,o1,o2];
+
+                        arr=Array.from(new Set(arr));
+                        arr1=Array.from(new Set(arr1));
+                        arr2=Array.from(new Set(arr2));
+
+                        result.text=`arr:${JSON.stringify(arr)}
+                        arr1:${JSON.stringify(arr1)}
+                        arr2:${JSON.stringify(arr2)}
+                        `;
+                    }
+                },
+            },        
         ],
     }
 ]
